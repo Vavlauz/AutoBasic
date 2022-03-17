15691c6385353dc804ae6daa410d82d7428877a3
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-03-17 19:59:28 +0700

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
         int expected = 1000;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -21,7 +21,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1);
         int expected = 999;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -30,7 +30,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(500);
         int expected = 500;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -39,7 +39,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(999);
         int expected = 1;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -48,7 +48,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -57,7 +57,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1001);
         int expected = 999;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -66,6 +66,6 @@ public class CashbackHackServiceTest {
         int actual = service.remain(2000);
         int expected = 1000;
 
-        assertEquals(actual, expected, "wrong bonus");
+        assertEquals(expected, actual);
     }
 }
